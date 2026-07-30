66fa16573223df8eb26088297fbbc33cdafe5b72
SajithNuski <sajithnuski555@gmail.com>
2026-07-30 23:57:51 +0530

    navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 // import React from 'react';
-import assets from "../assets/assets";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>
-      <img src={assets.logo} alt="Logo" />
+    <div className="flex justify-between items-center py-3 mx-4 lg:mx-44">
+      <img className="w-32 sm:mx-44" src={assets.logo} alt="Logo" />
       <button>
         Get Started
         <img src={assets.arrow_icon} alt="Arrow Icon" />
