6446254bb14e2657cc2298b835e654dc2e3f1fb8
SajithNuski <sajithnuski555@gmail.com>
2026-08-03 23:26:32 +0530

    add the footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        {/* footer */}
+        <p>
+            {/* write footer content here */}
+            developed by sajith nuski   
+            &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+        </p>
+    </div>
+  )
+}
