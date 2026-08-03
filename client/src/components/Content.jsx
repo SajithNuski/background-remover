436ae551318196df19c8975d781709837584a308
SajithNuski <sajithnuski555@gmail.com>
2026-08-03 23:28:35 +0530

    content add

---
+++ client/src/components/Content.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const Content = () => {
+  return (
+    <div>
+        {/* Content goes here */}
+        <p>This is the main content area.</p>
+        
+    </div>
+  )
+}
+
+export default Content
