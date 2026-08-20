5f25bbb5cd2a25c32451a83ea8a93564a79e4946
SajithNuski <sajithnuski555@gmail.com>
2026-08-20 23:50:30 +0530

    Home Right section responsive

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,18 +7,30 @@ const Header = () => {
       {/* left */}
       <div className="">
         <h1 className="text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-700 leading-tight">
-          Remove the <br /> <span className="bg-gradient-to-r from-violet-600 to-fuchsia-500 text-transparent bg-clip-text">background</span> from <br />
+          Remove the <br className="max-md:hidden" />{" "}
+          <span className="bg-gradient-to-r from-violet-600 to-fuchsia-500 text-transparent bg-clip-text">
+            background
+          </span>{" "}
+          from <br className="max-md:hidden" />
           images for free
         </h1>
         <p className="my-6 text-[15px] text-neutral-500">
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry.
-          <br /> Lorem Ipsum has been the industry's standard dummy text ever.
+          <br className="max-sm:hidden" /> Lorem Ipsum has been the industry's
+          standard dummy text ever.
         </p>
         <div>
           <input type="file" name="" id="upload1" hidden />
-          <label className="inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r  from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700" htmlFor="upload1">
-            <img className="w-4" src={assets.upload_btn_icon} alt="Upload Icon" />
+          <label
+            className="inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r  from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700"
+            htmlFor="upload1"
+          >
+            <img
+              className="w-4"
+              src={assets.upload_btn_icon}
+              alt="Upload Icon"
+            />
             <p className="text-white text-sm">Upload Image</p>
           </label>
         </div>
